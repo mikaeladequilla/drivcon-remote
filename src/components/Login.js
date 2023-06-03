a21ae4d3e1ad87a5804d1a3661b4df0b566a11c1
mikaeladequilla <mikaela.dequilla@gmail.com>
2023-06-03 11:03:01 +0800

    Fourth commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { Link, useNavigate } from 'react-router-dom';
-import whiteLogo from '../drivcon_white.png';
-import logo from '../drivcon_logo.png';
 
+// Firebase configuration for your application
 const firebaseConfig = {
     apiKey: 'AIzaSyB7tlY9bDGWxRPVTcPlZ8WbjMMyunIb5vE',
     authDomain: 'drivcon-f00f7.firebaseapp.com',
@@ -12,64 +11,65 @@ const firebaseConfig = {
     // ... other Firebase configurations
 };
 
+// Initialize Firebase if it hasn't been already
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
+// Login component
 const Login = ({ onLogin }) => {
+    // State variables for login form inputs and error messages
     const [loginEmail, setLoginEmail] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
     const [loginError, setLoginError] = useState('');
+
+    // useNavigate hook to allow for redirection after successful login
     const navigate = useNavigate();
 
+    // Listen for changes in the user's authentication state
     useEffect(() => {
         const unsubscribe = firebase.auth().onAuthStateChanged(user => {
             if (user) {
-                onLogin(); // Update parent component
-                navigate('/portal'); // Navigate to portal
+                // If a user is logged in, run the onLogin function and redirect to the portal page
+                onLogin();
+                navigate('/portal');
             }
         });
 
-        // Clean up subscription on unmount
+        // Clean up the subscription to prevent memory leaks
         return () => unsubscribe();
     }, [navigate, onLogin]);
 
-    const handleLoginEmailChange = (e) => {
-        setLoginEmail(e.target.value);
-    };
-
+    // Handle form input changes to update state variables
+    const handleLoginEmailChange = (e) => setLoginEmail(e.target.value);
     const handleLoginPasswordChange = (e) => {
         const input = e.target.value;
         if (/^\d{0,6}$/.test(input)) {
             setLoginPassword(input);
         }
     };
-
     const handleKeyPress = (e) => {
         const charCode = e.which ? e.which : e.keyCode;
         if (charCode < 48 || charCode > 57) {
           e.preventDefault();
         }
-      };
+    };
 
+    // Handle form submission for login
     const handleLogin = (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Prevent the form from reloading the page
         firebase
-            .auth()
-            .signInWithEmailAndPassword(loginEmail, loginPassword)
+            .auth() // Get the Firebase authentication service
+            .signInWithEmailAndPassword(loginEmail, loginPassword) // Sign in the user with the provided email and password
             .then((userCredential) => {
-                // Handle successful login
+                // This block is executed when the login is successful
                 const user = userCredential.user;
                 console.log('Logged in:', user);
-
-                // Call the onLogin prop to update the parent component
                 onLogin();
-
-                // Navigate to the portal
                 navigate('/portal');
             })
             .catch((error) => {
-                // Handle login error
+                // This block is executed if there's an error during login
                 console.error(error);
                 setLoginError('Invalid email or password. Please try again.');
             });
